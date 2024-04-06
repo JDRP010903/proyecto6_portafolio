6e26cce97bea68737042b8111a53ca15b0cb6b2b
JorgeRea <danireaprado@hotmail.com>
2024-04-06 01:50:04 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import 'boxicons/css/boxicons.min.css'
+
+const App = () => {
+
+  return (
+    <>
+      HOLA
+    </>
+  )
+}
+
+export default App
